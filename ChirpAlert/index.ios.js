8f84e887fa1959aac7281fe54a56d9cff5338f4f
William Johnson <wjohnson1000@gmail.com>
2016-02-17 13:42:07 -0700

    routing implmented

--- ChirpAlert/index.ios.js
+++ ChirpAlert/index.ios.js
@@ -8,103 +8,35 @@ import React, {
 	AppRegistry,
 	Component,
 	LinkingIOS,
+  Navigator,
+  NavigatorIOS,
 	StyleSheet,
 	Text,
 	TouchableHighlight,
+  TouchableOpacity,
 	View
 } from 'react-native';
 
-class ChirpAlert extends Component {
-	componentDidMount() {
-		LinkingIOS.addEventListener('url', this._handleOpenURL);
-	}
-	componentWillUnmount() {
-		LinkingIOS.removeEventListener('url', this._handleOpenURL);
-	}
-	_handleOpenURL(event) {
-		var token = event.url.replace('chirpalert://&token=', '');
-		AsyncStorage.setItem("token", token);
-	}
-	_onPressButton() {
-		LinkingIOS.openURL(
-			'http://127.0.0.1:3000/auth/login/twitter'
-		);
-	}
-	_onPressOtherButton() {
-		AsyncStorage.getItem("token").then(function(token){
-			console.log(token);
-			fetch('http://127.0.0.1:3000/test', {
-				method: 'GET',
-				headers: {
-					'Authorization': 'Bearer ' + token
-				}
-			})  
-			.then(response => console.log(response))
-			.catch((error) => {
-				console.warn(error);
-			});
+var LandingPage = require('./routes/landingpage');
 
-		});
-	}
-
-	render() {
-		return (
-			<View style={styles.container}>
-			<Text style={styles.welcome}>
-			Welcome to React Native!
-			</Text>
-			<TouchableHighlight style={styles.button}
-			underlayColor='#99d9f4' onPress={this._onPressButton}>
-			<Text style={styles.buttonText}>Go</Text>
-			</TouchableHighlight>
-			<TouchableHighlight style={styles.button}
-			underlayColor='#99d9f4' onPress={this._onPressOtherButton}>
-			<Text style={styles.buttonText}>Go</Text>
-			</TouchableHighlight>
-			<Text style={styles.instructions}>
-			To get started, edit index.ios.js
-			</Text>
-			<Text style={styles.instructions}>
-			Press Cmd+R to reload,{'\n'}
-			Cmd+D or shake for dev menu
-				</Text>
-			</View>
-		);
-	}
-}
-
-const styles = StyleSheet.create({
-	container: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		flex: 2,
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
-	buttonText: {
-		color: 'white',
-	},
-	button: {
-		height: 36,
-		width: 36,
-		flex: 1,
-		backgroundColor: '#48BBEC ',
-		borderColor: '#48BBEC ',
-		borderWidth: 1,
-		borderRadius: 8,
-		marginBottom: 10,
-		alignSelf: 'stretch',
-		justifyContent: 'center'
-	}
+var ChirpAlert = React.createClass({
+    render: function() {
+        return (
+            <NavigatorIOS
+                style={styles.navigationContainer}
+                initialRoute={{
+                title: "Navigator Example",
+                component: LandingPage,
+            }} />
+        );
+    }
 });
-
-AppRegistry.registerComponent('ChirpAlert', () => ChirpAlert);
+ 
+var styles = StyleSheet.create({
+    navigationContainer: {
+        flex: 1
+    }
+});
+ 
+AppRegistry.registerComponent("ChirpAlert", () => ChirpAlert);
+//module.exports = ChirpAlert;
