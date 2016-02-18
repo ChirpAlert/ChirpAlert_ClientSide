4195cea54d39a103676a94137ab8b2245ba1f895
annahuffman147 <annahuffman.plc@gmail.com>
2016-02-17 20:21:54 -0700

    fix typo, but still broken

--- ChirpAlert/routes/searchResults.js
+++ ChirpAlert/routes/searchResults.js
@@ -81,38 +81,7 @@ class searchResults extends Component {
   }
 
 }
-//   constructor(props) {
-//     super(props);
-//     var dataSource = new ListView.DataSource(
-//       {rowHasChanged: (r1, r2)=>r1.guid !==r2.guid}
-//     );
-//     this.state = {
-//       dataSource: dataSource.cloneWithRows(this.props.listings)
-//     };
-//   }
-//
-//   renderRow(rowData, sectionID, rowID) {
-//     return (
-//       <TouchableHighlight
-//         underlayColor = '#dddddd'>
-//         <View>
-//           <Text>{newbird.englishName}</Text>
-//         </View>
-//       </TouchableHighlight>
-//     );
-//   }
-//
-  // render() {
-  //   return (
-  //     <View style={styles.box}>
-  //       <Text>
-  //         hello there
-  //       </Text>
-  //     </View>
-  //   );
-  // }
-// }
-//
+
 const styles = StyleSheet.create({
   wikiLink: {
     fontSize: 12,
